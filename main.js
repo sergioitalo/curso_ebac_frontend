1ece4a29269d46aac66404432c6cf4a91a64b996
Sérgio Ítalo <sergioitalo.network@gmail.com>
2023-05-28 20:12:13 -0300

    arquivos JS atualizado

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-const formulario_italo = document.getElementsByTagName('form')
+const validacaoForm = function (a, b) {
+  const number1 = a
+  const number2 = b
+  parseFloat(number1, number2)
+  const resposta = number2 > number1
+  return resposta
+}
 
-formulario_italo.addEventListener('submit', function(evento){
+const formulario = document.querySelector('form')
+console.log(formulario)
+
+formulario.addEventListener('submit', function(evento) {
   evento.preventDefault()
 
+  const inputA = document.querySelector('.numberA')
+  const inputB = document.querySelector('.numberB')
 
+  if (validacaoForm(inputA.value, inputB.value)) {
+    const sucessoMsg = document.querySelector('.mensagem_sucesso')
+    sucessoMsg.style.display = 'block'
+
+    inputA.value = ''
+    inputB.value = ''
+
+  } else {
+    const erroMsg = document.querySelector('.mensagem_erro')
+    erroMsg.style.display = 'block'
+
+  }
 })
+
+
